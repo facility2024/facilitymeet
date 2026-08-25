2cf011e7a3cfaab814d9b5d79bdabf322c17f04f
FacilityMeet <facility2024@users.noreply.github.com>
2026-08-25 18:55:11 -0300

    fix(build): increase webpack asset size limit to 5MB

--- webpack.config.js
+++ webpack.config.js
@@ -348,7 +348,7 @@ module.exports = (_env, argv) => {
                 })
             ],
 
-            performance: getPerformanceHints(perfHintOptions, 3.5 * 1024 * 1024) },
+            performance: getPerformanceHints(perfHintOptions, 5 * 1024 * 1024) },
         { ...config,
             entry: {
                 'alwaysontop': './react/features/always-on-top/index.tsx'
